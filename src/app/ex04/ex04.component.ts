abfadf0316115f31f2d1b980a405373b4de0d7bb
Weverton <wevertonrodriguesarantes@gmail.com>
2022-07-05 11:18:35 -0300

    Removendo CSS

--- src/app/ex04/ex04.component.ts
+++ src/app/ex04/ex04.component.ts
@@ -6,7 +6,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-ex04',
   templateUrl: './ex04.component.html',
-  styleUrls: ['./ex04.component.css']
 })
 export class Ex04Component implements OnInit {
 
