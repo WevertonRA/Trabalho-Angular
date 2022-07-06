855cbc19eea0a93f29aaee8ff2465ef1a1c179d2
Weverton <wevertonrodriguesarantes@gmail.com>
2022-07-06 07:35:54 -0300

    Services

--- src/app/service/exs.service.ts
+++ src/app/service/exs.service.ts
@@ -13,8 +13,20 @@ export class ExsService {
 
   url = environment.urlServer;
 
+  create(value: ExServer): Observable<ExServer> {
+    return this.http.post<ExServer>(`${this.url}/list`, value)
+  }
+
   update(value: ExServer): Observable<ExServer> {
     return this.http.put<ExServer>(`${this.url}/list/${value.id}`, value)
+  }
+
+  delete(value: ExServer): Observable<ExServer> {
+    return this.http.delete<ExServer>(`${this.url}/list/${value.id}`)
+  }
+
+  findAll(id: any): Observable<ExServer> {
+    return this.http.get<ExServer>(`${this.url}/list`)
   }
 
   findById(id: any): Observable<ExServer> {
